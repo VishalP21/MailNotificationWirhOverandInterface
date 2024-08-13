14bc3eb09ab350a35ec09feae356697bd17ad3b1
Vishal Prajapati <vishalp951295@gmail.com>
2024-08-13 11:29:35 +0530

    Add project files.

---
+++ MailNotificationWirhOverandInterface/Program.cs
@@ -0,0 +1,6 @@
+﻿using MailNotificationWirhOverandInterface;
+
+mailNotification mailNotification = new mailNotification();
+
+mailNotification.SetEmailConfiguration();
+mailNotification.SendNotification("vishalp951295@gmail.com","hi","hello");
---
+++ MailNotificationWirhOverandInterface/SMTPConfrig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailNotificationWirhOverandInterface
+{
+    public class SMTPConfrig
+    {
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public bool EnableSsl { get; set; }
+
+        public string FromEmail { get; set; }
+
+        public string FromPassword { get; set; }
+
+
+        public virtual void SetEmailConfiguration()
+        {
+            Host = "smtp.gmail.com";
+            Port = 587;
+            EnableSsl = true;
+            FromPassword = "aoll ijcd ityg matl";
+            FromEmail = "vishalp951295@gmail.com";
+
+        }
+
+    }
+}
